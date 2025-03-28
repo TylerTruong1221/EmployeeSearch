55fa18afa8dd44105c00848e57288a871ad34555
Tyler Truong <tylerttruong@cpp.edu>
2025-03-28 16:22:26 -0700

    Initialized EmployeeSearch

---
+++ EmployeeMain.c
@@ -0,0 +1,34 @@
+//Compile using the following command: gcc EmployeeMain.c EmployeeTable.c EmployeeOne.c
+
+//gcc EmployeeMain.c EmployeeTable.c EmployeeTwo.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Employee.h"
+int main(void) {
+    //defined in employeeSearchOne.c
+    PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
+    PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char *nameToFind);
+
+    //definted in EmployeeTable.c
+    extern Employee EmployeeTable[];
+    extern const int EmployeeTableEntries;
+
+    PtrToEmployee matchPtr; //Declaration
+    matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045);
+
+    //Example not found
+    if (matchPtr != NULL)
+        printf("Employee ID 1045 is in record %ld\n", matchPtr - EmployeeTable);
+    else
+        printf("Employee ID is NOT found in the record\n");
+
+    //Example found
+    matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat");
+    if (matchPtr != NULL)
+        printf("Employee Tony Bobcat is in record %ld\n", matchPtr - EmployeeTable);
+    else
+        printf("Employee Tony Bobcat is NOT found in the record\n");
+    
+    return EXIT_SUCCESS;
+}
